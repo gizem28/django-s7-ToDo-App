671b71f7c6d55bf6e2e81b9effad6f25a927e84b
gizem28 <gizemodun28@gmail.com>
2022-02-03 02:12:12 +0100

    fixed

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,11 @@
+
 from django.urls import path
-from .views import home
+from .views import home, todo_list, todo_add, todo_update, todo_delete
 
 urlpatterns = [
     path("", home, name="home"),
+    path("list/", todo_list, name="list"),
+    path("add/", todo_add, name="add"),
+    path("update/<int:id>/", todo_update, name="update"),
+    path("delete/<int:id>/", todo_delete, name="delete"),
 ]
